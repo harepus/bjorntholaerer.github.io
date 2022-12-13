6084a7487039b477da203a6ea8c70bd2dae5ab25
bjorntholaerer <116543861+bjorntholaerer@users.noreply.github.com>
2022-12-14 00:32:51 +0100

    Add files via upload

---
+++ login_js/js/mittscript.js
@@ -0,0 +1,44 @@
+/* Kommentarer i 
+   JavaScript kan 
+   gå over flere linjer	   
+*/
+	
+// eller bare en enkelt linje 
+
+
+// Nå skal vi definere en funksjon som henter variabler fra HTML-form
+
+function variablerFraForm(){
+	
+	// det er god praksis å deklarere lokale variabler (variabler som benyttes inne i en funksjon) i starten av funksjonen
+	
+	let variabel_brukernavn ="";
+	let variabel_passord ="";
+	let myOutput = "";
+	let returVerdi = "";
+
+	// tilordne lokale variabler med verdier fra skjema/form
+	// vi bruker document.getElementById(id/variabelnavn_i_form)
+
+	variabel_brukernavn = document.getElementById("brukernavn");
+	variabel_passord = document.getElementById("passord");
+
+	
+	// console.log brukes for utvikling/debugging
+	// output til console.log i browser inspector (ctrl/cmd+i)
+
+	// vis variablene i konsoll-loggen
+/*	console.log("Brukernavn : " + variabel_brukernavn);
+	console.log("Passord : " + variabel_passord);
+*/	
+	// lage det som skal vises i html-filen i variabelen myOutput
+	//
+
+	myOutput = "Hei " + variabel_brukenavn + " !"input_2";
+	
+	// vise output i en spesifikk paragraph <p id="output"> i html-filen	
+	document.getElementById("output").innerHTML = myOutput;	
+	
+	// en funksjon kan returnere en verdi
+	return(returVerdi)
+}
