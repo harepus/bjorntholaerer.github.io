6d3ff7e99c99602451a8984c54fb6758d18e1d69
bjorntholaerer <116543861+bjorntholaerer@users.noreply.github.com>
2022-12-14 01:20:29 +0100

    Update mittscript.js

--- 1im/login_js/js/mittscript.js
+++ 1im/login_js/js/mittscript.js
@@ -20,21 +20,20 @@ function variablerFraForm(){
 	// tilordne lokale variabler med verdier fra skjema/form
 	// vi bruker document.getElementById(id/variabelnavn_i_form)
 
-	variabel_brukernavn = document.getElementById("brukernavn");
-	variabel_passord = document.getElementById("passord");
+	variabel_brukernavn = document.getElementById("brukernavn").value;
+	variabel_passord = document.getElementById("passord").value;
 
 	
 	// console.log brukes for utvikling/debugging
 	// output til console.log i browser inspector (ctrl/cmd+i)
-
 	// vis variablene i konsoll-loggen
-/*	console.log("Brukernavn : " + variabel_brukernavn);
+	console.log("Brukernavn : " + variabel_brukernavn);
 	console.log("Passord : " + variabel_passord);
-*/	
-	// lage det som skal vises i html-filen i variabelen myOutput
+	
+	// legge til det som skal vises i html-filen i variabelen myOutput
 	//
 
-	myOutput = "Hei " + variabel_brukenavn + " !"input_2";
+	myOutput = "Hei " + variabel_brukernavn + " !";
 	
 	// vise output i en spesifikk paragraph <p id="output"> i html-filen	
 	document.getElementById("output").innerHTML = myOutput;	
